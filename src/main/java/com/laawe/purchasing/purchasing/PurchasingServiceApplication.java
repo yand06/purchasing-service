058981917a5bac4aad13eac583db6df625785c58
SUPRIYANDI LA AWE <supriyandi.awe@indivaragroup.com>
2026-03-24 12:53:41 +0700

    [MAIN] - ADD FILE FROM SPRING INITIALIZR

---
+++ src/main/java/com/laawe/purchasing/purchasing/PurchasingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.laawe.purchasing.purchasing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchasingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurchasingServiceApplication.class, args);
+	}
+
+}
